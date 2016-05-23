e07212428b47214cfa0d8f75b06cee22ee061178
yhung119 <yhung119@illinois.edu>
2016-05-23 21:39:57 +0800

    interview Questions

---
+++ list/intervewQuestion.py
@@ -0,0 +1,55 @@
+'''
+2.1 Remove Dups:
+	Write code to remove duplicates from an unsorted linked list
+	FOLLOW UP:
+		How would you solve this problem if a temporary buffer is not allowed?
+'''
+
+
+
+'''
+2.2 Return Kth to Last:
+	Implement an algroithm to find the kth to last element of a singly linked list.
+
+'''
+
+
+'''
+2.3 Delete Middle Node:
+	Implement an algorithm to delete a node in the middle of a dsingly linked list
+	given only access to that node.
+	EX:
+	Input: the node c from the linked list a->b->c->d->e
+	Output: nothing is returned but the new linked list is a->b->d->e
+'''
+
+
+'''
+2.4 Partition:
+	Write code to partition a linked list around a value x, such that all nodes less than x 
+	come before all node greater than or equal to x. If x is contained within the list, the 
+	values of x only need to be after the elements less than x
+	Ex:
+	Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
+	Ouput: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+'''
+
+'''
+2.5 Sum List
+
+
+'''
+
+'''
+2.6 Palindrome: Implement a function to check if a linked list is a palindrome.
+'''
+
+
+'''
+2.7 Intersection
+'''
+
+'''
+2.8 Loop Detection:
+'''
+
